68a1d4056ca58e73f4273f0eb2dd386c31fc3ec8
Bada cornelius <95828899+Bada11@users.noreply.github.com>
2025-08-29 10:51:46 +0100

    Revert "Auth"

--- src/components/NavbarServer.tsx
+++
@@ -1,9 +0,0 @@
-import { auth } from "@/auth";
-import Navbar from "./Navbar";
-
-const NavbarServer = async () => {
-  const session = await auth();
-  return <Navbar session={session} />;
-};
-
-export default NavbarServer;
--- src/components/PostForm.tsx
+++ src/components/PostForm.tsx
@@ -3,13 +3,6 @@ import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Card } from "./ui/card";
 
 const PostForm = () => {
-  const loop = () => {
-    const arr: number[] = [1, 2, 3, 4, 5, 6];
-
-    for (let i = 0; i < arr.length; i++) {
-      console.log(i);
-    }
-  };
   return (
     <>
       <Card className="w-full lg:max-w-[800px]">
@@ -19,7 +12,7 @@ const PostForm = () => {
             <AvatarFallback>CN</AvatarFallback>
           </Avatar>
           <Link href="/post" className="text-blue-500 hover:underline">
-            You've got something in mind? Create Post
+            You got something in mind? Create Post
           </Link>
         </div>
       </Card>
